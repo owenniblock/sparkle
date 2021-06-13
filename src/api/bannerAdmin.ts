e089890a43286261deee127ec6eef875ec1ac5bd
Oleksii Pohorelyi <oleksii.pohorelyi@gmail.com>
2021-06-13 05:57:38 +0300

    discussion_r644714568

--- src/api/bannerAdmin.ts
+++ src/api/bannerAdmin.ts
@@ -1,16 +1,18 @@
 import firebase from "firebase/app";
 import { BannerFormData } from "types/banner";
 
+export interface makeUpdateBannerProps {
+  venueId: string;
+  banner?: BannerFormData;
+}
+
 export const makeUpdateBanner = async ({
   venueId,
   banner,
-}: {
-  venueId: string;
-  banner: BannerFormData;
-}): Promise<void> => {
+}: makeUpdateBannerProps): Promise<void> => {
   const params = {
     venueId,
-    banner,
+    banner: banner ?? firebase.firestore.FieldValue.delete(),
   };
 
   await firebase.functions().httpsCallable("venue-adminUpdateBannerMessage")(
